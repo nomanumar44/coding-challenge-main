2dcffb8415aa915a46a78ee0a4c95baf06d4e948
nomanumar44 <91877679+nomanumar44@users.noreply.github.com>
2023-02-28 11:41:29 -0800

    changing in route provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
             Route::middleware('web')
                 ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
+                ->group(base_path('routes/userConnection.php'));
         });
     }
 
